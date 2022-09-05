9951e66dfc7684150326611938288adf403ccffb
Regina <reginabb68@gmail.com>
2022-09-05 13:34:23 +0800

    Start with create-react-app

--- client/src/helpers/constants.js
+++ client/src/helpers/constants.js
@@ -1,3 +1,5 @@
-export const CONNECTION_PORT = "http://localhost:3001/";
+//export const CONNECTION_PORT = "http://localhost:3001/";
+export const CONNECTION_PORT = "https://chatapp-server-01.herokuapp.com";
 
-export const API_HOST = "http://localhost:3001";
+//export const API_HOST = "http://localhost:3001";
+export const API_HOST = "https://chatapp-server-01.herokuapp.com";
